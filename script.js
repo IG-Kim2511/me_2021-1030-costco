43bdaa398f786eba9938b26286946f4da5a1ebda
iikim <iikim2511@gmail.com>
2021-10-30 15:04:11 -0400

    .left-second-container

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/* 
+(🍀home----------)
+
+🍀 section  
+ -> 🍉 small section 
+🍚 : next plan
+🍄 : code explain
+🍖 js-6 :connect
+
+🍄Algorithm)  
+
+🍖 :connect
+😈 bug
+
+(🍤 delete later) : delete later
+🍤 (hard-coding) (soft-coding)
+🍤 way-1 way-2 way-3
+🍤 :  Tip
+*/
+
+// 🍀let
+
+// 🍀const     
+const scannerInput = document.querySelector('.scanner-input');
+
+
+// 🍀js0928-20. load. when the browser start, focus on scan-input
+window.addEventListener('DOMContentLoaded',()=>{    
+
+    // keyboardLogin
+
+    // 🍉real code
+    // keyboardLogin.classList.add('change');	
+	// loginDisplay.value = "";
+	// keyboardDisplay.value = "";    
+    // scannerInput.style.display =`none`;  
+
+
+    // 🍉test
+    scannerInput.focus();
+
+});
+
